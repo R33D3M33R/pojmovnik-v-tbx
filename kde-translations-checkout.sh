08ade2acd63174b4e371eabcf9362dfd7e1b19da
Andrej Mernik <R33D3M33R@users.noreply.github.com>
2016-07-31 13:19:51 +0200

    Add script for smart KDE translation checkout

---
+++ kde-translations-checkout.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Avtor: Andrej Mernik, 2016, http://andrej.mernik.eu/
+#Licenca: GPLv3
+
+# osnovna mapa
+svn checkout --depth empty svn+ssh://svn@svn.kde.org/home/kde/ svn
+svn update --set-depth empty svn/trunk
+svn update --set-depth empty svn/branches
+svn update --set-depth empty svn/branches/stable
+
+# predloge za KDE4 trunk
+svn update --set-depth empty svn/trunk/l10n-kde4
+svn update --set-depth empty svn/trunk/l10n-kde4/templates
+svn update --set-depth infinity svn/trunk/l10n-kde4/templates/messages
+
+# prevodi za KDE4 trunk
+svn update --set-depth empty svn/trunk/l10n-kde4/sl
+svn update --set-depth infinity svn/trunk/l10n-kde4/sl/messages
+
+# predloge za KDE4 stable
+svn update --set-depth empty svn/branches/stable/l10n-kde4
+svn update --set-depth empty svn/branches/stable/l10n-kde4/templates
+svn update --set-depth infinity svn/branches/stable/l10n-kde4/templates/messages
+
+# prevodi za KDE4 stable
+svn update --set-depth empty svn/branches/stable/l10n-kde4/sl
+svn update --set-depth infinity svn/branches/stable/l10n-kde4/sl/messages
+
+# predloge za KF5 trunk
+svn update --set-depth empty svn/trunk/l10n-kf5
+svn update --set-depth empty svn/trunk/l10n-kf5/templates
+svn update --set-depth infinity svn/trunk/l10n-kf5/templates/messages
+
+# prevodi za KDE4 trunk
+svn update --set-depth empty svn/trunk/l10n-kf5/sl
+svn update --set-depth infinity svn/trunk/l10n-kf5/sl/messages
+
+# predloge za KF5 stable
+svn update --set-depth empty svn/branches/stable/l10n-kf5
+svn update --set-depth empty svn/branches/stable/l10n-kf5/templates
+svn update --set-depth infinity svn/branches/stable/l10n-kf5/templates/messages
+
+# prevodi za KDE4 stable
+svn update --set-depth empty svn/branches/stable/l10n-kf5/sl
+svn update --set-depth infinity svn/branches/stable/l10n-kf5/sl/messages
